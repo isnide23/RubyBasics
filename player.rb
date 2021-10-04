d175be274d22727f56c5bc3227639845be6ca400
Ian Snyder <isnide23@gmail.com>
2021-10-03 21:00:19 -0700

    add population array and print a list of every member with score

--- player.rb
+++ player.rb
@@ -1,13 +1,25 @@
 class Person
     
-    attr_reader :name, :score
+    attr_accessor :name, :score
 
-    def initialize(name, score = 0)
+    def initialize(name = "Player", score = rand(10..300))
         @name = name
         @score = score
     end
 
-
-
 end
+
+population_array = (1..50).map do |p|
+    person = Person.new
+    person.name = "Player#{p}"
+    person
+end
+# puts population_array[0].score
+
+population_array.each do |person|
+    puts "Ready #{person.name}! Score: #{person.score}"
+end
+
+
+
 
